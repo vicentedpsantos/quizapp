0d502616b1428c245cbe1d60857a9cc8bb2150da
Vicente Santos <vicentedpsantos@gmail.com>
2023-05-28 21:23:59 +0100

    Make image slightly transparent

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -13,14 +13,15 @@ class StartScreen extends StatelessWidget {
   Widget build(context) {
     return Center(
       child: Column(mainAxisSize: MainAxisSize.min, children: [
-        Image.asset('assets/images/quiz-logo.png', width: 300),
+        Image.asset('assets/images/quiz-logo.png',
+            width: 300, color: const Color.fromARGB(150, 255, 255, 255)),
         const SizedBox(height: 80),
         const StyledText('Learn Flutter the fun way!', 28.0, Colors.white),
         const SizedBox(height: 30),
         OutlinedButton.icon(
           onPressed: startQuiz,
           style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-          icon: Icon(Icons.arrow_right_alt),
+          icon: const Icon(Icons.arrow_right_alt),
           label: const Text('Start Quiz!'),
         ),
       ]),
