d82ccc858b550242a38c640c3c857e370e5dca04
Vicente Santos <vicentedpsantos@gmail.com>
2023-05-29 19:09:27 +0100

    Trying to add questions and answers to screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:quizapp/data/questions.dart';
+import 'package:quizapp/styled_text.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
@@ -10,11 +13,29 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestion = 0;
+
+  const exampleAnswers = ['Resposta1', 'Resposta2', 'Resposta3', 'Resposta4'];
+
+  List<Widget> generateResponsesForQuestion(int currentQuestion) {
+    return [
+      Text('Resposta1'),
+      Text('Resposta2'),
+      Text('Resposta3'),
+      Text('Resposta4')
+    ];
+  }
+
   @override
   Widget build(context) {
     return const Center(
       child: Column(mainAxisSize: MainAxisSize.min, children: [
-        Text('Questions Screen'),
+        Text(
+          'Pergunta aqui',
+          style: TextStyle(
+              fontSize: 28.0, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        generateResponsesForQuestion(currentQuestion)
       ]),
     );
   }
