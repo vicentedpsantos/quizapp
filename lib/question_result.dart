d61eb174fca32f0bbce7510ffe6ff8c7efea63c4
Vicente Santos <vicentedpsantos@gmail.com>
2023-05-30 18:48:33 +0100

    App works!

---
+++ lib/question_result.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:quizapp/models/quiz_question.dart';
+
+class QuestionResult extends StatelessWidget {
+  const QuestionResult(this.question, this.answerGiven, {super.key});
+
+  final QuizQuestion question;
+  final String answerGiven;
+
+  bool correctAnswer() {
+    return question.getCorrectAnswer() == answerGiven;
+  }
+
+  @override
+  Widget build(context) {
+    return  ColoredBox(
+      color: Colors.green,
+      child: Material(
+        child: ListTile(
+          leading: correctAnswer() ? Icon(Icons.check) : Icon(Icons.close),
+          title: Text(question.getText()),
+          subtitle: Text("Answer given: $answerGiven, correct answer: $question.getCorrectAnswer()")
+        ),
+      ),
+    );
+  }
+}
--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'package:quizapp/data/questions.dart';
 import 'package:quizapp/models/quiz_question.dart';
+import 'package:quizapp/question_result.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen(this.switchToScreen, Map answers, {super.key});
+  const ResultScreen(this.switchToScreen, this.answers, {super.key});
 
+  final Map answers;
   final void Function(String screenName) switchToScreen;
 
   List<Widget> buildResult() {
-    List<Widget> results = answers.map((questionId, answerGiven) {
-       QuizQuestion question = questions.firstWhere((quizQuestion) => quizQuestion.getId() == questionId);
+    List<Widget> results = [];
+
+    answers.forEach((questionId, answerGiven) {
+      QuizQuestion question = questions.firstWhere((quizQuestion) => quizQuestion.getId() == questionId);
+
+      results.add(QuestionResult(question, answerGiven));
     });
 
     return results;
